7d51915634eba3e1fa40a404a750166e8dcb1124
JuanCardonaB <juanjocarbol@gmail.com>
2025-10-08 00:33:39 -0500

    Refactor load_data method to set column names for the DataFrame

--- notebooks/data_cleaning.py
+++ notebooks/data_cleaning.py
@@ -8,6 +8,15 @@ class Data_Cleaning:
     def load_data(self, path):
         try:
             df_readed = pd.read_csv(Path(path))
+            df_readed.columns = [
+                "car_name",
+                "displacement_l",
+                "engine_type",
+                "horsepower",
+                "transmission_speed",
+                "top_speed_mph",
+                "cost_usd"
+            ]
             self.df = df_readed
         except Exception as e:
             print(e)
